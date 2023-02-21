57849df7c01bdadeb561fa48fe0e4dc8ca62f5fc
Invirent <74411024+Invirent@users.noreply.github.com>
2023-02-21 19:28:25 +0700

    [FIX] Missing javascript

--- resources/views/frontend/candidate-form.blade.php
+++ resources/views/frontend/candidate-form.blade.php
@@ -38,11 +38,38 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <title>Form</title>
+    <style>
+        .container-fluid{
+          margin-top: 100px;
+        }
+
+        .border{
+          border: black 1px solid;
+        }
+
+        .form-page{
+          background-color: #0f7bb1;
+          padding: 20px;
+          border-radius: 5px;
+          border: black 1px solid;
+        }
+
+        .form-color {
+            background-color: #6ad13d;
+            padding: 20px;
+            border-radius: 5px;
+            border: black 1px solid;
+        }
+    </style>
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row mx-0 justify-content-center">
-          <div class="col-md-7 col-lg-5 px-lg-2 col-xl-4 px-xl-0 px-xxl-3">
+          <div class="col-md-7 col-lg-5 px-lg-2 col-xl-4 px-xl-0 px-xxl-3 ">
+            <div class="form-page">
+            <div class="form-color">
+              <h4>Hello {{ $candidate->name }}, please complete the question.</h4>
+            </div>
             <form
               method="POST"
               class="w-100 rounded-1 p-4 border bg-white"
@@ -59,7 +86,7 @@
                 <select required name="quiz_{{ $quiz->id }} " class="form-select">
                     @foreach($CandidateAnswerSelection as $answer)
                         @if($answer->quiz_id == $quiz->id)
-                            <option value="{{ $answer->id }}">{{ $answer->answer }}</option>
+                            <option type="radio" value="{{ $answer->id }}">{{ $answer->answer }}</option>
                         @endif
                     @endforeach
                 </select>
@@ -71,6 +98,7 @@
               </div>
             </form>
           </div>
+        </div>
         </div>
       </div>
       
